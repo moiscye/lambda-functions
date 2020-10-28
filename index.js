de2410b68c8a8c6149a2796ade2ceaf5c25b10db
Moises <moy_085@hotmail.com>
2020-10-28 17:28:46 +1100

    Update resizeImages function

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
-const { downloadImage, resize, saveToS3 } = require("./utils");
+const { downloadImageFromS3, resize, saveToS3 } = require("./utils");
 
 exports.handler = async (event) => {
+  console.log("event.Records.length", event.Records.length);
   let filesProcessed = event.Records.map(async (record) => {
-    let bucket = record.s3.bucket.name;
-    let filename = record.s3.object.key;
-
-    // Get file from S3
-    var params = {
-      Bucket: bucket,
-      Key: filename,
-    };
-    let inputData = await s3.getObject(params).promise();
-    const buf = Buffer.from(inputData, "binary");
-    let targetFilename =
-      filename.substring(0, filename.lastIndexOf(".")) + "-small.jpg";
-
+    const buf = await downloadImageFromS3(record);
     const resized = await resize(buf, 100, 100);
-    const key = await saveToS3(bucket, targetFilename, resized);
+    const key = await saveToS3(bucket + "-dest", targetFilename, resized);
     return { key };
   });
 
   await Promise.all(filesProcessed);
-  console.log("done");
-  return "done";
+  console.log("DONE");
+  return "DONE";
 };
--- utils.js
+++ utils.js
@@ -3,12 +3,23 @@ const gm = GM.subClass({ imageMagick: true });
 const FileType = require("file-type");
 const AWS = require("aws-sdk");
 const s3 = new AWS.S3();
-
 const axios = require("axios");
 
-exports.downloadImage = async (url) => {
+exports.downloadImageFromURL = async (url) => {
   const res = await axios.get(url, { responseType: "arraybuffer" });
   return Buffer.from(res.data, "binary");
+};
+
+exports.downloadImageFromS3 = async (source) => {
+  const bucket = source.s3.bucket.name;
+  const filename = source.s3.object.key;
+  const params = {
+    Bucket: bucket,
+    Key: filename,
+  };
+  const inputData = await s3.getObject(params).promise();
+
+  return Buffer.from(inputData.Body, "binary");
 };
 
 exports.resize = async (buf, width, height) => {
