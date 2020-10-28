fac23910f522cb66024ae5744a01283b34c7d39a
Moises <moy_085@hotmail.com>
2020-10-28 18:15:52 +1100

    Update working version before refactoring

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-const { downloadImageFromS3, resize, saveToS3 } = require("./utils");
+const AWS = require("aws-sdk");
+AWS.config.update({ region: "ap-southeast-2" });
+const s3 = new AWS.S3();
+const { resize, saveToS3 } = require("./utils");
 
 exports.handler = async (event) => {
-  console.log("event.Records.length", event.Records.length);
   let filesProcessed = event.Records.map(async (record) => {
-    const buf = await downloadImageFromS3(record);
+    let bucket = record.s3.bucket.name;
+    let filename = record.s3.object.key;
+
+    // Get file from S3
+    var params = {
+      Bucket: bucket,
+      Key: filename,
+    };
+    console.log("PARAMS", params);
+    let inputData = await s3.getObject(params).promise();
+    const buf = Buffer.from(inputData.Body, "binary");
+    let targetFilename =
+      filename.substring(0, filename.lastIndexOf(".")) + "-small";
+
     const resized = await resize(buf, 100, 100);
     const key = await saveToS3(bucket + "-dest", targetFilename, resized);
     return { key };
   });
 
   await Promise.all(filesProcessed);
-  console.log("DONE");
-  return "DONE";
+  console.log("done");
+  return "done";
 };
